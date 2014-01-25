1263b99eb290c4dbf38d7206638f2202c435bd18
Ankit Goyal <ankit3goyal@gmail.com>
2014-01-24 23:22:15 -0600

    adding bc file with changed test file

--- spring2014-compilers/assignment2/LocalOpts/inputFiles/algebraic.c
+++ spring2014-compilers/assignment2/LocalOpts/inputFiles/algebraic.c
@@ -1,10 +1,11 @@
 int compute (int a, int b)
 {
   int result = (a/a);
-
+  unsigned int t = (b/b);
   result *= (b/b);
   result += (b-b);
   result /= result;
   result -= result;
+  result = b + 0;
   return result;
 }

64687c662ebbf8226b7c6a96376db1b94a7f363a
Ankit Goyal <ankit3goyal@gmail.com>
2014-01-26 23:57:23 -0600

    adding strength reduction and removing buggy identities working

--- spring2014-compilers/assignment2/LocalOpts/LocalOpts.cpp
+++ spring2014-compilers/assignment2/LocalOpts/LocalOpts.cpp
@@ -35,42 +35,29 @@
 
 STATISTIC(NumXForms, "The # of algebric identies transformations performed");
 STATISTIC(NumConstFolds, "The # of constant folding operations");
+STATISTIC(NumStrReductions, "The # of strength reductions");
 
 using namespace llvm;
 
 namespace {
 
-    struct ConstantFolding : public InstVisitor<ConstantFolding> {
-        void visitStoreInst(StoreInst &I){ 
-            Value* str_ptr  = I.getPointerOperand();
-            Value* v= I.getValueOperand();
-            if(isa<Constant>(*v)){
-                for(Value::use_iterator ui = str_ptr -> use_begin(), e = str_ptr->use_end(); ui != e; ++ui){
-                    if(LoadInst *pinst = dyn_cast<LoadInst>(*ui)){
-                        pinst->replaceAllUsesWith(v);
-                        ++NumConstFolds;
-                    }
-                }
-            }
-        }
-    };
-
 
     template<typename APType, typename ConstantType>
-        Value * algIdentityAS(Instruction &i, APType generalZeroOne){
+        Value * algIdentityAS(Instruction &i, APType generalZeroOne, int operation){
             errs() << i << "\n";
             Value *operand1 = i.getOperand(0);
             Value *operand2 = i.getOperand(1);
             /* ConstantInt and ConstantFP. http://llvm.org/docs/doxygen/html/Constants_8h.html */
             /* Constant types */
             ConstantType *cint; 
+
             if(cint = dyn_cast<ConstantType>(operand2)){
                 if(id_compare<ConstantType, APType>(*cint, generalZeroOne))
-                    return cint;
+                    return operand1;
             }
-            else if(cint = dyn_cast<ConstantType>(operand1)){
+            else if((cint = dyn_cast<ConstantType>(operand1)) && operation!=1){
                 if(id_compare<ConstantType, APType>(*cint, generalZeroOne))
-                    return cint;
+                    return operand2;
             }
             return NULL;
         }
@@ -131,14 +118,30 @@ namespace {
 
 
     void makeTheChanges(BasicBlock::iterator &ib, Value* val){
-        ++NumXForms;
         Instruction* bb = ib;
         ReplaceInstWithValue(bb->getParent()->getInstList(), ib,val);
     }
 
-    void reduce_strength(BasicBlock::iterator &ib, ConstantInt &cint, unsigned reduction_factor){
-        errs() << "Reducing strength";
+    void reduce_strength(BasicBlock::iterator &ib, ConstantInt &cint, Value &other, unsigned opcode){
+        ++NumStrReductions;
+        APInt apint = cint.getValue();
+        unsigned reduction_factor = apint.logBase2();
+        BinaryOperator* shift_instruction ;
+        if(opcode == Instruction::Add)
+            shift_instruction = BinaryOperator::Create( Instruction::Shl, &other, ConstantInt::get(cint.getType(), reduction_factor, false));
+        else
+            return;
+        //shift_instruction = BinaryOperator::Create( Instruction::AShr, &other, ConstantInt::get(cint.getType(), reduction_factor, false));
+        Instruction *ii= ib;
+        ii->getParent()->getInstList().insertAfter(ib, shift_instruction);
+        makeTheChanges(ib, shift_instruction);
     }
+    bool can_reduce_strength(ConstantInt& tcint){
+        APInt op1 = tcint.getValue();
+        if(op1.isPowerOf2()) return true;
+        return false;
+    }
+
 
     void optimize(Instruction *ii, BasicBlock::iterator& i, Type *instructionType){
         Value *v;
@@ -146,86 +149,81 @@ namespace {
         intype = dyn_cast<IntegerType>(instructionType);
 
         switch(ii->getOpcode()){
-            case Instruction::Store:{ //note the explicit block here. Otherwise variables can't be defined inside case since they are in scope for other but not initialized
-                                        ConstantFolding CF;
-                                        CF.visit(cast<StoreInst>(ii));
-                                        break;
-                                    }
             case Instruction::Add: 
-                                    v = algIdentityAS<APInt, ConstantInt>(*ii, getZeroOne<APInt>(intype->getBitWidth(),0));
-                                    if(v) makeTheChanges(i, v);
-                                    break; //X+0 = 0+X = X  
-            case Instruction::FAdd:
-                                    v = algIdentityAS<APFloat, ConstantFP>(*ii, getZeroOne<APFloat>(0,0)); 
-                                    if(v) makeTheChanges(i, v);
-                                    break; //X+0 = 0+X = X
+                v = algIdentityAS<APInt, ConstantInt>(*ii, getZeroOne<APInt>(intype->getBitWidth(),0),0);
+                if(v) { 
+                    makeTheChanges(i, v);                                             
+                    ++NumXForms;
+                }
+
+                break; //X+0 = 0+X = X  
             case Instruction::Sub: 
-                                    v = algIdentityAS<APInt, ConstantInt>(*ii, getZeroOne<APInt>(intype->getBitWidth(),0));  
-                                    if(v) makeTheChanges(i, v);
-                                    break; //X-0 = X; 0-X=-X
-            case Instruction::FSub: 
-                                    v = algIdentityAS<APFloat, ConstantFP>(*ii, getZeroOne<APFloat>(0,0));  
-                                    if(v) makeTheChanges(i, v);
-                                    break; //X-0 = X; 0-X=-X
-            case Instruction::Mul:
-                                    v = algIdentityMD<APInt, ConstantInt>(*ii, getZeroOne<APInt>(intype->getBitWidth(),1), 0, 1);  //X * 1 = X , 1 * X = X; operation = 0 identity = 1
-                                    if(v) { makeTheChanges(i, v); break;}
-                                    else if (v = algIdentityMD<APInt, ConstantInt>(*ii, getZeroOne<APInt>(intype->getBitWidth(),0), 0, 0)){ //X * 0 = 0, 0 * X = 0 OP=0 ID = 0
-                                        makeTheChanges(i, v); break; }
+                v = algIdentityAS<APInt, ConstantInt>(*ii, getZeroOne<APInt>(intype->getBitWidth(),0), 1);  
+                if(v) { 
+                    makeTheChanges(i, v); 
+                    ++NumXForms;
+                }
+                break; //X-0 = X;
+            case Instruction::Mul:{
+                                      v = algIdentityMD<APInt, ConstantInt>(*ii, getZeroOne<APInt>(intype->getBitWidth(),1), 0, 1);  //X * 1 = X , 1 * X = X; operation = 0 identity = 1
+                                      if(v) { 
+                                          makeTheChanges(i, v); 
+                                          ++NumXForms;
 
-                                    break; //X*1 = X; 1*X=X
-            case Instruction::FMul:
-                                    v = algIdentityMD<APFloat, ConstantFP>(*ii, getZeroOne<APFloat>(0,1), 0, 1);  //OP=0 ID=1
-                                    if(v) { makeTheChanges(i, v); break ; }
-                                    else if(v = algIdentityMD<APFloat, ConstantFP>(*ii, getZeroOne<APFloat>(0,1), 0, 0))//OP=0 ID=0
-                                    { makeTheChanges(i, v); break ;}
-                                    break; //X*0 = X; 0*X=-X //NEED TO CHECK THIS CASE
+                                      }
+
+                                      else if (v = algIdentityMD<APInt, ConstantInt>(*ii, getZeroOne<APInt>(intype->getBitWidth(),0), 0, 0)){ //X * 0 = 0, 0 * X = 0 OP=0 ID = 0
+                                          makeTheChanges(i, v); 
+                                          ++NumXForms;
+                                      }
+                                      break; //X*1 = X; 1*X=X
+                                  }
+
             case Instruction::UDiv:
-            case Instruction::SDiv:
-                                    if(v = algIdentityMD<APInt, ConstantInt>(*ii, getZeroOne<APInt>(intype->getBitWidth(),1),1,1))// X / 1 = X; OP=1 ID=1
-                                    {makeTheChanges(i, v); break;}
-                                    else if(v = algIdentityMD<APInt, ConstantInt>(*ii, getZeroOne<APInt>(intype->getBitWidth(),0),1,0)) // X / 1 = X OP=1 ID=0
-                                    {      
-                                        makeTheChanges(i,v); break;
-                                    }
-
-                                    break;
-            case Instruction::FDiv:
-                                    if(v = algIdentityMD<APFloat, ConstantFP>(*ii, getZeroOne<APFloat>(0,1),1,1))// X / 1 = X; OP=1 ID=1
-                                    {makeTheChanges(i, v); break;}
-                                    else if(v = algIdentityMD<APFloat, ConstantFP>(*ii, getZeroOne<APFloat>(0,0),1,0)) // X / 1 = X OP=1 ID=0
-                                    {      
-                                        makeTheChanges(i,v); break;
-                                    }
-
-                                    break;
-
+            case Instruction::SDiv:{
+                                       if(v = algIdentityMD<APInt, ConstantInt>(*ii, getZeroOne<APInt>(intype->getBitWidth(),1),1,1))// X / 1 = X; OP=1 ID=1
+                                       {
+                                           makeTheChanges(i, v); 
+                                           ++NumXForms;
+                                       }
+                                       else if(v = algIdentityMD<APInt, ConstantInt>(*ii, getZeroOne<APInt>(intype->getBitWidth(),0),1,0)) // X / 1 = X OP=1 ID=0
+                                       {      
+                                           makeTheChanges(i,v); 
+                                           ++NumXForms;
+                                       }
+                                       break;
+                                   }
+            default: break;
         }               
-
 
         //Constant Folding
         if(ii->getNumOperands() == 2 && isa<Constant>(ii->getOperand(0)) && isa<Constant>(ii->getOperand(1))){
- //           v = fold_constants(op, ii->getOperand(0), ii->getOperand(1));
-   //         makeTheChanges(i,v);
+            //           v = fold_constants(op, ii->getOperand(0), ii->getOperand(1));
+            //         makeTheChanges(i,v);
         }
 
         //strength reduction
-        if(ii->getOpcode() == Instruction::Mul){
-            ConstantInt *op1;
-            if(op1 = dyn_cast<ConstantInt>(ii->getOperand(0)) && op1.isPowerOf2()){
-                  unsigned reduction_factor = op1.logBase2();
-                  reduce_strength(i, op1, reduction_factor);
-            }
+
+        switch(ii->getOpcode()){
+            case Instruction::UDiv:
+            case Instruction::SDiv:
+            case Instruction::Mul:{
+                                      ConstantInt *tcint;
+                                      Value *other;
+                                      if((tcint = dyn_cast<ConstantInt>(ii->getOperand(0))) && can_reduce_strength(*tcint)){
+                                          other = ii->getOperand(0);
+                                          reduce_strength(i, *tcint, *other,ii->getOpcode());
+                                      }              
+                                      else if((tcint = dyn_cast<ConstantInt>(ii->getOperand(1))) && can_reduce_strength(*tcint)){
+                                          other = ii->getOperand(0);
+                                          reduce_strength(i, *tcint, *other,ii->getOpcode());
+                                      }
+                                      break;
+                                  }
+            default: break;
         }
+
     }
-
-    /*    void tryConstantFolding(Instruction *ii, Value *bb_it){
-          errs() << "CONSTANT: " << *ii << "\n";
-          if(ii->getNumOperands() == 2 && isa<Constant>(ii->getOperand(0)) && isa<Constant>(ii->getOperand(1)))
-          errs() << *ii << "\n" << "constant folding  here\n";
-          }
-     */
-
     class LocalOpts : public FunctionPass{
 
         public:
@@ -261,7 +259,7 @@ namespace {
                     runOnBasicBlock(*i);
                     //print_basic_block_info(*i);
                 }
-                return false;
+                return true;
             }
 
 
--- spring2014-compilers/assignment2/LocalOpts/generic_methods.h
+++ spring2014-compilers/assignment2/LocalOpts/generic_methods.h
@@ -3,13 +3,11 @@ using namespace llvm;
 namespace {
     /* Arbitrary precision constant values. To take care of both 32 bit and 64 bit numbers. */
     template <typename APType> APType getZeroOne(unsigned num_bits, int zeroOne);
-    template <> APFloat getZeroOne(unsigned num_bits, int zeroOne) { return APFloat((float)zeroOne); }
     template <> APInt   getZeroOne(unsigned num_bits, int zeroOne) { return APInt(num_bits,zeroOne); }
 
     /* Compare value based on the type values to identify algebraic identities */
     /* generalize compare method based on type */
     template <typename ConstantType, typename APType> bool id_compare(ConstantType& cint, APType zero);
-    template <> bool id_compare(ConstantFP& cint, APFloat zero) { return cint.getValueAPF().compare(zero) == APFloat::cmpEqual; }
     template <> bool id_compare(ConstantInt& cint, APInt zero) { return cint.getValue().eq(zero); }
 
 }

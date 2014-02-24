979c50257b593f3dabe61839f9aa68697e1fffe5
Ankit Goyal <ankit3goyal@gmail.com>
2014-02-23 20:01:42 -0600

    initial licm

--- spring2014-compilers/assignment4/sum.c
+++ spring2014-compilers/assignment4/sum.c
@@ -1,8 +1,20 @@
 int sum (int a, int b){
+
     int i,x;
-    x = 23;
+    if(a > 10)
+        return 0;
+    else
+        x = 97;
+
     for(i=0; i<a; i++){
+        if(i<7)
+            x = 23;
+        else
+            x = 78;
+        a = 43;
+        b= 24 * 2324;
         x = x + 1;
     }
+
     return b;
 }
